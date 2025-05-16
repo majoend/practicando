bab8d1e87daffd4ba9400d2c7ad73c2676e07eb2
majoend <majojanuary32@gmail.com>
2025-05-15 21:02:27 -0400

    hi.

---
+++ majo.py
@@ -0,0 +1,12 @@
+
+
+edad = int(input('Ingresa tu edad: '))
+
+if edad <= 18:
+    print('Buena edad')
+
+
+elif 18 < edad < 30:
+    print('Eres un joven aún')
+elif 30 < edad < 40:
+    print('Estas en una edad excelente')
